846e0eee341fb33f2479f86cb5929e0ae68d532b
rramadhan00 <nomoreagain1@gmail.com>
2018-05-07 20:09:42 +0700

    get table testing

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\anjing;
 
 Route::get('fuck', function() {
-    return [
-        'message' => 'ini adalah fuck',
-    ];
+
+    return anjing::all();
+
 });
