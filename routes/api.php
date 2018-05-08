83db957ff355cd16c0de0f0533d9d114844cc693
rramadhan00 <nomoreagain1@gmail.com>
2018-05-09 00:26:34 +0700

    add route get-create-delete

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,27 @@
 use Illuminate\Http\Request;
 use App\anjing;
 
-Route::get('fuck', function() {
+Route::get('all', function() {
 
     return anjing::all();
+
+});
+
+Route::get('all/{id}', function(anjing $id) {
+
+    return $id;
+
+});
+
+Route::post('all', function() {
+
+    return anjing::create(request()->all());
+
+});
+
+Route::delete('all/{id}', function(anjing $id){
+
+    $id->delete();
+    return 'Sukses menghapus data';
 
 });
